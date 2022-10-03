010baf2885ae0543705eabbbfc05cf72953ec571
Ahyeon Jeon <ahyeonjeon@Ahyeons-MBP.attlocal.net>
2022-10-03 13:30:17 -0700

    add: eslint

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 function phoneNumber(strnum) {
-    const listNums = strnum.toString().split('')
-    if (listNums.length > 10) {
-        return 'This is invalid phone number. The number should be 10 digit numbers.'
-    }
-    else {
-        const firstThree = listNums.slice(0, 3).join('')
-        const restFour = listNums.slice(3,6).join('')
-        const lastFour = listNums.slice(6,10).join('')
+  const listNums = strnum.toString().split('');
+  if (listNums.length > 10) {
+    return 'This is invalid phone number. The number should be 10 digit numbers.';
+  }
+  const firstThree = listNums.slice(0, 3).join('');
+  const restFour = listNums.slice(3, 6).join('');
+  const lastFour = listNums.slice(6, 10).join('');
 
-        return '(' + firstThree + ') ' + restFour + '-' + lastFour
-        }
-    }
+  return `(${firstThree}) ${restFour}-${lastFour}`;
+}
 
 // const result = phoneNumber(1234567890)
 // console.log(result)
 
-module.exports = phoneNumber
+module.exports = phoneNumber;
